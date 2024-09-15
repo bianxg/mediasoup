3fb1f9dcdd4bc98323ff1af7c9aa1110cc381f07
bianxg <bianxg@gmail.com>
2024-09-15 20:49:46 +0800

    Add info level log

--- node/src/Logger.ts
+++ node/src/Logger.ts
@@ -4,22 +4,26 @@ const APP_NAME = 'mediasoup';
 
 export class Logger {
 	readonly #debug: debug.Debugger;
+	readonly #info: debug.Debugger;
 	readonly #warn: debug.Debugger;
 	readonly #error: debug.Debugger;
 
 	constructor(prefix?: string) {
 		if (prefix) {
 			this.#debug = debug(`${APP_NAME}:${prefix}`);
+			this.#info = debug(`${APP_NAME}:${prefix}`);
 			this.#warn = debug(`${APP_NAME}:WARN:${prefix}`);
 			this.#error = debug(`${APP_NAME}:ERROR:${prefix}`);
 		} else {
 			this.#debug = debug(APP_NAME);
+			this.#info = debug(`${APP_NAME}:INFO`);
 			this.#warn = debug(`${APP_NAME}:WARN`);
 			this.#error = debug(`${APP_NAME}:ERROR`);
 		}
 
 		/* eslint-disable no-console */
 		this.#debug.log = console.info.bind(console);
+		this.#info.log = console.info.bind(console);
 		this.#warn.log = console.warn.bind(console);
 		this.#error.log = console.error.bind(console);
 		/* eslint-enable no-console */
@@ -27,6 +31,10 @@ export class Logger {
 
 	get debug(): debug.Debugger {
 		return this.#debug;
+	}
+
+	get info(): debug.Debugger {
+		return this.#info;
 	}
 
 	get warn(): debug.Debugger {
@@ -37,3 +45,4 @@ export class Logger {
 		return this.#error;
 	}
 }
+
